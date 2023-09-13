c04ee8253a32fe289cb2d047afa9960403ba275a
Naman Jain <naman1205jain@gmail.com>
2023-09-13 10:33:21 -0700

    starcoder model name

--- inference/scripts/starcoder_humaneval.sh
+++ inference/scripts/starcoder_humaneval.sh
@@ -1,5 +1,5 @@
 python main.py \
---model bigcode/gpt_bigcode-starcoder --use_auth_token \
+--model bigcode/starcoder --use_auth_token \
 --trust_remote_code --tasks humaneval --batch_size 20 --n_samples 20 \
 --max_sequence_length 1024 --precision bf16 --temperature 0.3 \
 --num_gpus 4 --exp_name starcoder_humaneval_03 --allow_code_execution --shuffle \
